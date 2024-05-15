f5245f1a6c0d1e5307b9bfa8596d2090fa06ead4
TFMV <mcgeehan@gmail.com>
2024-05-15 14:32:12 -0500

    scrap

--- ArrowLake/scrap/sandbox/dis.go
+++ ArrowLake/scrap/sandbox/dis.go
@@ -7,18 +7,18 @@ import (
 	"time"
 )
 
-// DiscordianErisian - A struct representing Erisian elements
+// DiscordianErisian represents Erisian elements.
 type DiscordianErisian struct {
 	Name   string
 	Number int
 }
 
-// LawOfFives - Checks if a number adheres to the Law of Fives
+// LawOfFives checks if a number adheres to the Law of Fives.
 func (de DiscordianErisian) LawOfFives() bool {
 	return de.Number%5 == 0
 }
 
-// String - Overrides the String method for custom printing
+// String overrides the String method for custom printing.
 func (de DiscordianErisian) String() string {
 	if de.LawOfFives() {
 		return fmt.Sprintf("Hail Eris! The number %d is sacred because it's a multiple of 5!", de.Number)
@@ -26,7 +26,7 @@ func (de DiscordianErisian) String() string {
 	return fmt.Sprintf("%d is just mundane.", de.Number)
 }
 
-// GenerateRandomErisian - Generates a DiscordianErisian with a random number
+// GenerateRandomErisian generates a DiscordianErisian with a random number.
 func GenerateRandomErisian() DiscordianErisian {
 	rand.Seed(time.Now().UnixNano())
 	return DiscordianErisian{Name: "Random Erisian", Number: rand.Intn(100)}
